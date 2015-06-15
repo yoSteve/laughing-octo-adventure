a330d1956fe3bf975bf708f8043f211bcaad4eda
goofiw <goofiw@gmail.com>
2015-06-15 10:18:58 -0700

    moved useOrCreate from user model to passport module

--- models/user.js
+++ models/user.js
@@ -15,30 +15,6 @@ var User = new Schema({
 	waiting: Boolean
 });
 
-
-
-User.statics.findOrCreate = function(profile, done) {
-	this.findOne({'password' : profile.id}, function(err, user) {
-		if (err)
-			return done(err)
-		if(user)
-			return done(null, user)
-	});
-
-	var newUser = this();
-	newUser.username = profile.displayName;
-	newUser.password = profile.id;
-	newUser.email = profile.name.familyName; //placeholder
-
-	newUser.save(function(err){
-		if (err)
-			return done(err);
-	});
-	return done(null, newUser);
-	
-	console.log('shouldnt get here');	
-}
-
 mongoose.model('User', User);
 module.exports = mongoose.model('User'); 
 
--- passport/google.js
+++ passport/google.js
@@ -9,9 +9,34 @@ module.exports = function(passport) {
 			callbackURL: "http://localhost:3000/auth/google/response"
 		},
 		function(iss, sub, profile, accessToken, refreshToken, done) {
-			User.findOrCreate(profile, function (err, user) {
-				return done(err, user);
-			});
+			
+			var findOrCreateUser = function(){
+				User.findOne({'password' : profile.id}, function(err, user) {
+					if (err)
+					return done(err)
+				if(user)
+					return done(null, user)
+				});
+				var newUser = new User();
+				//set credentials
+				newUser.username = profile.displayName;
+				newUser.password = profile.id;
+				newUser.email = profile.name.familyName; //placeholder 
+
+				//save user
+				newUser.save(function(err) {
+					if (err){
+						console.log('Error in Saving user: ' + err);
+						return done(null, newUser);
+					}
+					console.log('User Registration succesful');
+					return done(null, newUser);
+				});
+			}
+			process.nextTick(findOrCreateUser);
+//			User.findOrCreate(profile, function (err, user) {
+//				return done(err, user);
+//			});
 		}
 	));
 }
