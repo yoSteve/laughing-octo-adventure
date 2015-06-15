737ed692b6ad3882a08d1028d9a3261a467cd021
goofiw <goofiw@gmail.com>
2015-06-14 22:52:19 -0700

    breaking up client server game logic, all in index will modularize later

--- public/javascript.js
+++ public/javascript.js
@@ -1,4 +1,361 @@
 
 $(function(){
 	var socket = io();
+	var map;
+  var canvas = document.createElement('canvas'),
+  ctx = canvas.getContext('2d');
+  $('#start').on('click', function() {
+  	console.log("I cliecked");
+  	socket.emit('start-game', function(board) {
+  	});
+  });
+  socket.on('game-board', function(data) {
+  	var board = data[0];
+  	var matches = data[1];
+  	paintBoard(board)
+	  var body = document.getElementById('game-board');
+	  body.appendChild(canvas);
+  });
+// Initialize the matrix.
+
+
+  canvas.width = 408;
+  canvas.height = 448;
+
+
+
+  ////////////// Gameplay Variables /////////////////
+      // take in variable map, and set up board for play
+      var PLAYER1_DEFAULT_HP = 50,
+          PLAYER2_DEFAULT_HP = 50,
+          ENEMY_DEFAULT_HP   = 50;
+
+      var player1HP = PLAYER1_DEFAULT_HP,
+          player2HP = PLAYER2_DEFAULT_HP,
+          enemyHP   = ENEMY_DEFAULT_HP;
+
+     // MANA POOL //
+     var  manaRed    = 0,
+          manaBlue   = 0,
+          manaGreen  = 0,
+          manaYellow = 0,
+          manaBlack  = 0,
+          manaWhite  = 0;
+
+      var meleRed    = 15,
+          meleBlue   = 15,
+          meleGreen  = 10,
+          meleYellow = 10,
+          meleBlack  =  5,
+          meleWhite  =  5;
+
+  //////////////// Game Play Order ////////////////////
+
+  
+  // updateManaPool();
+  // updateEnemyHP();
+
+  // boardSetup(board);
+      // assignCrystalsToBoard(board);
+      // refreshBoard(board); // includes findAllMatches()
+      // zeroAllMana();
+      // setHPtoDefault();
+      // paintBoard(board);
+
+  // BEGIN TURNS LOOP
+
+      // player1Turn
+          // **useManaSpell   // debits mana pool and updates enemy HP or player hp
+      //     getMoveFromUser(map); // includes move logic. includes paintBoard()
+      //     refreshBoard(map);  // awards mana to player and does mele attacks to enemy
+      //     updateManaPool();
+      //     updateEnemyHP(); // prints enemyHP to browser
+      //     paintBoard(map);    
+      // // end turn
+
+      // // player2Turn
+      //     // useManaSpell   // debits mana pool and updates enemy HP or player hp
+      //     getMoveFromUser(map); // includes move logic. includes paintBoard()
+      //     refreshBoard(map);  // awards mana to player and does mele attacks to enemy
+      //     updateManaPool();
+      //     updateEnemyHP(); // prints enemyHP to browser 
+      //     paintBoard(map);    
+      // end turn
+
+  //SET WIN-LOSE CONDITIONS TO BREAK LOOP
+          // alert loser
+          // alert winner & award prizes to winner
+
+  // boardSetup(map);
+  // LoopPlayersTurn(map, enemyHP);
+      
+
+  ////////////// Turn Logic and Win Condition //////////////
+
+  function LoopPlayersTurn(board, enemyHealth) {
+      do {
+          player1Turn(enemyHealth);
+          // player2Turn(player2);
+      } while (enemyHP > 0);
+      if (player1HP > 0) {
+          alert("Player 1 wins");
+      } else { 
+          alert("Player 1 loses");
+      }
+      // if (player2HP > 0) {
+      //     alert("Player 2 wins");
+      // } else {
+      //     alert ("Player 2 loses")
+      // }
+  }
+
+  function player1Turn(board, enemyHealth){
+      getMoveFromUser(board);
+      findAllMatches(board);
+      paintBoard(board);
+  } 
+
+  ///////////// Score Logic Functions /////////////////
+
+  // function awardMana(cell) {
+  //     switch (cell) {
+  //         case 0 :
+  //             manaRed++;
+  //             break;
+  //         case 1 :
+  //             manaBlue++;
+  //             break;
+  //         case 2 :
+  //             manaGreen++;
+  //             break;
+  //         case 3 :
+  //             manaYellow++;
+  //             break;
+  //         case 4 :
+  //             manaBlack++;
+  //             break;
+  //         case 5 :
+  //             manaWhite++;
+  //             break;
+  //     }
+  // }
+  //shouldn't need, start game with 0 mana
+  // function zeroAllMana() {
+  //     manaRed    = 0,
+  //     manaBlue   = 0,
+  //     manaGreen  = 0,
+  //     manaYellow = 0,
+  //     manaBlack  = 0,
+  //     manaWhite  = 0;    
+  // }
+
+  function updateManaPool() {
+      $("#mana-red").text(mana[0]);
+      $("#mana-blue").text(mana[1]);
+      $("#mana-green").text(mana[2]);
+      $("#mana-yellow").text(mana[3]);
+      $("#mana-black").text(mana[4]);
+      $("#mana-white").text(mana[5]);
+  }
+
+  function meleAttack(cell) {
+      switch (cell) {
+          case 0 :
+              enemyHP = enemyHP - meleRed;
+              break;
+          case 1 :
+              enemyHP = enemyHP - meleBlue;
+              break;
+          case 2 :
+              enemyHP = enemyHP - meleGreen;
+              break;
+          case 3 :
+              enemyHP = enemyHP - meleYellow;
+              break;
+          case 4 :
+              enemyHP = enemyHP - meleBlack;
+              break;
+          case 5 :
+              enemyHP = enemyHP - meleWhite;
+              break;
+      }
+  }
+
+  function updateEnemyHP() {
+      $("#enemy-hp").text(enemyHP);
+  }
+
+  function setHPtoDefault() {
+      enemyHP = ENEMY_DEFAULT_HP;
+      playerHP = PLAYER1_DEFAULT_HP;
+  }
+
+  ///////////// Move Logic Functions /////////////////
+
+  var iColumn,
+      iRow,
+      fColumn,
+      fRow;
+
+  function getInitialCoords() {
+      var initialPos = prompt("Enter inital Position", "col,row");
+      iColumn = initialPos.slice(0,1);
+      iRow = initialPos.slice(-1);
+  }
+
+  function getFinalCoords() {
+      var finalPos = prompt("Enter final Position", "col, row");
+      fColumn = finalPos.slice(0,1);
+      fRow = finalPos.slice(-1);
+
+  }
+
+  function getMoveFromUser(board) {
+      getInitialCoords(),
+      getFinalCoords();
+
+      var Hmove = iColumn - fColumn,
+          Vmove = iRow - fRow;
+
+      console.log("Hmove: " + Hmove);
+      console.log("Vmove: " + Vmove);
+
+      if (Hmove < 0 ) {
+          Hmove = (-1 * Hmove);
+          rotateRight(board, iRow, Hmove);
+          paintBoard(board);
+      } else if (Hmove > 0) {
+          rotateLeft(board, iRow, Hmove);
+          paintBoard(board);
+      } else if (Vmove < 0) {
+          Vmove = (-1 * Vmove);
+          rotateDown(board[iColumn], Vmove);
+          paintBoard(board);
+      } else if (Vmove > 0) {
+          rotateUp(board[iColumn], Vmove);
+          paintBoard(board);
+      }
+  }
+
+
+  //takes intial & final coords, determines direction & distance of movement
+  // function defineMovement(iCol, iRow, fCol, fRow) {
+  //     var horDist = iCol - fCol;
+  //     var vertDist = iRow - fRow;
+  //     if (horDist < 0) {
+  //         console.log("moving right " + horDist + " spaces.");
+  //     } else if (horDist > 0) {
+  //         console.log("moving left " + horDist + " spaces.");
+  //     } else if (vertDist < 0 ) {
+  //         console.log("moving down " + vertDist + " spaces.") 
+  //     } else if (vertDist > 0) {
+  //         console.log("moving up " + vertDist + " spaces.")
+  //     }
+  // }
+
+
+  function findRow(board, row) {
+      // row is row index of board coordinates (board[col][row])
+      var array = [];
+      for (i = 0; i < SIZE; i++){
+          array.push(board[i][row]);
+      }
+      console.log("findRow looks like: " + array);
+      return array;
+  }
+
+  
+  // moves is number of spaces by which to shift values of array
+
+  function rotateLeft(board, row, moves) {
+      var array = findRow(board, row);
+      console.log("inside rotateLeft. array: " + array);
+      for (var i = 0; i < moves; i++) {
+          var beg = array.shift();
+          array.push(beg);
+      }
+      for (var i = 0; i < SIZE; i++) {
+          board[i][row] = array[i];
+      }
+  }
+
+  function rotateRight(board, row, moves) {
+      var array = findRow(board, row);
+      for (var i = 0; i < moves; i++) {
+          var end = array.pop();
+          array.unshift(end); 
+      }
+       for (var i = 0; i < SIZE; i++) {
+          board[i][row] = array[i];
+      }
+  }
+
+
+  // column should be given as map[col] where col is the target column
+  function rotateUp(column, moves) {
+      for (var i = 0; i < moves; i++) {
+          var beg = column.shift();
+          column.push(beg);
+      }
+  }
+
+  function rotateDown(column, moves) {
+      for (var i = 0; i < moves; i++) {
+          var end = column.pop();
+          column.unshift(end);
+      }
+  }
+
+
+
+
+  ///////////// Board Logic Functions ////////////////
+
+  function boardSetup(board){
+      zeroAllMana();
+      setHPtoDefault();
+      paintBoard(board);
+  }
+
+  function paintBoard(board) {
+      // Clear the canvas
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+      // Loops cycle through board. Switch picks color as per value of cell.
+      for (var row = 0; row < board.length; row++) {
+          for (var col = 0; col < board[0].length; col++) {
+              switch (board[row][col]) {
+                  case 0 :
+                      ctx.fillStyle = 'red';
+                      ctx.fillRect(row * 50, col * 50 + 20, 48, 48);
+                      break;
+                  case 1 : 
+                      ctx.fillStyle = 'blue'; 
+                      ctx.fillRect(row * 50, col * 50 + 20, 48, 48); 
+                      break;         
+                  case 2 :
+                      ctx.fillStyle = 'green';
+                      ctx.fillRect(row * 50, col * 50 + 20, 48, 48);
+                      break;          
+                  case 3 :
+                      ctx.fillStyle = 'yellow';
+                      ctx.fillRect(row * 50, col * 50 + 20, 48, 48);
+                      break;          
+                  case 4 :
+                      ctx.fillStyle = 'black';
+                      ctx.fillRect(row * 50, col * 50 + 20, 48, 48);
+                      break;          
+                  case 5 :
+                      ctx.fillStyle = 'floralwhite';
+                      ctx.fillRect(row * 50, col * 50 + 20, 48, 48);
+                      break;          
+                  default:
+                      ctx.fillStyle = 'grey';
+                      ctx.fillRect(row * 50, col * 50 + 20, 48, 48);          
+              }
+          }
+      } console.log("painting the board", board);
+  }
+
+
+
 });
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
-
+var SIZE = 8;
+var matches = new Array(6);
+var mana = new Array(6);
 var isAuthenticated = function(req, res, next) {
 	//checks to see if session is authenticated and
 	//lets through to their request
@@ -51,7 +53,189 @@ module.exports = function(passport, io){
 		socket.on('disconnect', function() {
 			console.log('somebody disconnected');
 		});
+		//start game board
+		socket.on('start-game', function(err) {
+			// if (err)
+			// 	return err;
+			console.log("we've made it this far");
+		  var gameBoard = new Array(SIZE);
+		  for (var i = gameBoard.length-1; i >= 0 ; i--) {
+	      gameBoard[i] = new Array(SIZE);
+  		}
+  		assignCrystalsToBoard(gameBoard);
+  		refreshBoard(gameBoard);
+  		mana.map(function(mana) { mana = 0 });
+			socket.emit('game-board', [gameBoard, mana]);
+		})
 	});
 	
 	return router;
+
+  function boardSetup(board){
+      assignCrystalsToBoard(board);
+      refreshBoard(board);
+      // zeroAllMana();
+      // setHPtoDefault();
+      // paintBoard(board);
+  }
+///////GAME BOARD LOGICE//////////////////
+  function getRandomCrystal() {
+      return Math.round(Math.random() * 5);
+  }
+
+  function assignCrystalsToBoard (board) {
+      for (var col = 0; col < board.length; col++) {
+          for (var row = 0; row < board[col].length; row++) {
+              if (board[col][row] == null) {
+                  board[col][row] = getRandomCrystal();
+              }
+          } 
+      }
+      return board;
+  }
+
+  function checkNullSpace(board) {
+    for (var col = SIZE -1; col >= 0; col--) {
+        var nullCount = 0;
+        for (var row = SIZE -1 ; row >= 0; row--) {
+            if (board[col][row] == null) {
+                nullCount++;
+            } else if (board[col][row] != null && nullCount > 0) { board[col][row + nullCount] = board[col][row];
+            }
+        } 
+        for (var i = 0; i <= nullCount-1; i++) {
+            board[col][i] = null;
+        }
+    } return board;
+  }
+
+  function dropNewCrystals(board) {
+      for (var col = 0; col < SIZE; col++) {
+          if (board[col][0] == null) {
+              board[col][0] = getRandomCrystal;
+          };
+      } console.log("inside dropNewCrystals");
+      return board;
+  }
+
+  function refreshBoard (board) {
+       for (var col = 0; col < SIZE; col++) {
+          for (var row = 0; row < SIZE; row++) {
+              do {
+                  findAllMatches(board);
+                  checkNullSpace(board);      
+                  assignCrystalsToBoard(board);
+              } while (board[col][row] == null);
+          } 
+      } 
+  }
+    ///////////// Match Logic Functions ////////////////
+
+  function findAllMatches(board) {
+      findBestMatchCol(board);
+      findBestMatchRow(board);
+      return board;
+  }
+
+ function findBestMatchRow (board) {
+      for (var col = 2; col < board.length; col++) {
+          for (var row = 0; row < board[col].length; row++) {
+              if ( findMatch3Row(board, col, row) ) {
+                  board[col-2][row] = null;
+                  board[col-1][row] = null;
+                  board[col][row]   = null;
+                  if ( findMatch4Row(board, col. row) ) {
+                      board[col-3][row] = null;
+                      if ( findMatch5Row(board, col, row) ) {
+                          board[col-4][row] = null; 
+                      }
+                  }
+              }  
+          } 
+      } 
+      return board;
+  }
+
+  function findMatch3Row(board, col, row) {
+      if ( board[col][row] == board[col-1][row] 
+          && board[col][row] == board[col-2][row] ) {
+              matches[(board[col][row])]++;
+              matches[(board[col-1][row])]++;
+              matches[(board[col-2][row])]++;
+              return true;
+      }
+      return false;
+  }
+
+  function findMatch4Row (board, col, row) {
+      if (col >= 3 
+          && findMatch3Row(board, col, row) 
+          && board[col][row] == board[col-3][row] ) {
+              matches[(board[col-3][row])]++;
+
+              return true;
+      }
+      return false;
+  }
+
+  function findMatch5Row (board, col, row) {
+      if ( col >= 4 
+      && findMatch4Row(board, col, row) 
+      && board[col][row] == board[col-4][row] ) {
+          matches[(board[col-4][row])]++;
+          return true;
+      }
+      return false;
+  }
+
+  function findBestMatchCol(board){
+      for (var col = 0; col < board.length; col++) {
+          for (var row = 2; row < board[col].length; row++) {
+              if ( findMatch3Col(board, col, row) ) {
+                  board[col][row-2] = null;
+                  board[col][row-1] = null;
+                  board[col][row]   = null;
+                  if ( findMatch4Col(board, col, row) ) {
+                      board[col][row-3] = null;
+                      if ( findMatch3Col(board, col, row) ) {
+                          board[col][row-4] = null;  
+                      }
+                  }
+              }
+          }
+      }
+      return board;
+  }
+
+
+  function findMatch3Col (board, col, row) {
+      if ( board[col][row] == board[col][row-1] 
+      && board[col][row] == board[col][row-2] ) {
+          matches[(board[col][row])]++;
+          matches[(board[col][row-1])]++;
+          matches[(board[col][row-2])]++;
+          return true;
+      }
+      return false;
+  } 
+
+  function findMatch4Col (board, col, row) {
+      if ( board[col][row] >= board[col][3] 
+      && findMatch3Col(board, col, row) 
+      && board[col][row] == board[col][row-3] ) {
+          matches[(board[col][row-3])]++;
+          return true;
+      }
+      return false;
+  }
+
+  function findMatch5Col (board, col, row) {
+      if ( board[col][row] >= board[col][4] 
+      && findMatch4Col(board, col, row) 
+      && board[col][row] == board[col][row-4] ) {
+          matches[(board[col][row-4])]++;
+          return true;
+      }
+      return false;
+  }
 }
