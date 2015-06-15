812d2bf1b3e7bab57ecbaf88e4c0a461beb725ce
Kevin Blues <kbluescode@gmail.com>
2015-06-15 12:17:29 -0700

    properly integrated engine

---
+++ game_js/resources.js
@@ -0,0 +1,12 @@
+game.resources = [
+    {
+        "name": "crystals-lg",
+        "type": "image",
+        "src": "data/img/crystals-lg.png"
+    },
+    {
+        "name": "crystals",
+        "type": "image",
+        "src": "data/img/crystals.png"
+    }
+];
