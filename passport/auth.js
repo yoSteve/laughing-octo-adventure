7fa81a1ed2aa992cb293c39ec411c2e3ac7a8266
goofiw <goofiw@gmail.com>
2015-06-20 20:08:09 -0700

    added gulp

--- passport/auth.js
+++ passport/auth.js
@@ -8,8 +8,7 @@ passport.serializeUser(function(user, done) {
   done(null, user.id);
 })
 
-passport.deserializeUser(function(id, done) {
-  User.findById(id, function(err, user) {
+passport.deserializeUser(function(id, done) { User.findById(id, function(err, user) {
     done(null, user);
   })
 })
