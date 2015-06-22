fd7a624c1c38459688bbd7dd15ac571a379fdb4c
goofiw <goofiw@gmail.com>
2015-06-21 17:39:43 -0700

    might be sending the board now

--- models/game.js
+++ models/game.js
@@ -21,7 +21,8 @@ GameSchema.statics.create = function (gameVars){
 //  game.home = JSON.parse(JSON.stringify(gameVars.homeUser.teams));
 //  game.away = JSON.parse(JSON.stringify(gameVars.awayUser.teams));
 //  game.home.hp = game.home.maxHp();
-//  game.away.hp = game.away.maxHp();
+//  game.away.hp = game.away.maxHp(); 
+  game.turn = game.randomPlayer();
   game.homeMana = 0;
   game.awayMana = 0;
   game.size = 8;
@@ -29,15 +30,122 @@ GameSchema.statics.create = function (gameVars){
   for (var i = game.board.length-1; i >= 0 ; i--) {
     game.board[i] = new Array(game.size);
   }    
+  game.setup(); 
   return game;  
 }
 
-GameSchema.methods.Board = function() {
-  
+GameSchema.methods.randomPlayer = function() {
+ 
 }
 
+//GameSchema.methods.refreshBoard = function() {
+//  //sends out new game board, hps, mana pools, and current player turn
+//    this.io.to(this.gameId).emit('refresh-board', {homeMana: this.homeMana,
+//      awayMana: this.awayMana,
+//      gameId: this.gameId, 
+//      home: this.awayUser.username, 
+//      away: this.homeUser.username, 
+//      gameBoard: this.board,
+//      turn: this.turn
+//    });
+//}
+
+module.exports = mongoose.model('Game', GameSchema, 'Game');
+
+
+GameSchema.methods.move = function(initPos, finPos) {
+  initialPos = initPos;
+  finalPos = finPos;
+  getMoveFromUser();
+  findAllMatches(gameBoard);
+  refreshBoard(gameBoard);
+  addMana();
+  zeroMatches();
+  console.log(mana);
+  return [gameBoard, mana];
+}
+
+GameSchema.methods.boardSetup = function(){
+      mana = new Array(6);
+      this.assignCrystalsToBoard();
+      this.refreshBoard();
+  }
+
+GameSchema.methods.zeroMana = function(mana){
+  	var i = 5;
+  	while(i >= 0){
+  		mana[i] = 0;
+  		i--;
+  	}
+  }
+
+GameSchema.methods.addMana =function() {
+  	var i = 5;
+  	while(i >= 0){
+      if (matches[i])
+    		mana[i] += matches[i];
+  		i--;
+    }
+  }
+ 
+
+GameSchema.methods.zeroMatches = function(){
+  	var i = 5;
+  	while(i >= 0){
+  		matches[i] = 0;
+  		i--;
+  	}
+  }
+  ///////GAME BOARD LOGIC//////////////////
+GameSchema.methods.getRandomCrystal = function() {
+      return Math.round(Math.random() * 5);
+  }
+
+GameSchema.methods.assignCrystalsToBoard = function() {
+      for (var col = 0; col < board.length; col++) {
+          for (var row = 0; row < board[col].length; row++) {
+              if (this.board[col][row] == null) {
+                  this.board[col][row] = getRandomCrystal();
+              }
+          } 
+      }
+  }
+
+GameSchema.methods.checkNullSpace = function() {
+    for (var col = SIZE -1; col >= 0; col--) {
+        var nullCount = 0;
+        for (var row = SIZE -1 ; row >= 0; row--) {
+            if (this.board[col][row] == null) {
+                nullCount++;
+            } else if (this.board[col][row] != null && nullCount > 0) { this.board[col][row + nullCount] = this.board[col][row];
+            }
+        } 
+        for (var i = 0; i <= nullCount-1; i++) {
+            this.board[col][i] = null;
+        }
+    } 
+  }
+
+GameSchema.methods.dropNewCrystals = function(board) {
+      for (var col = 0; col < SIZE; col++) {
+          if (board[col][0] == null) {
+              board[col][0] = getRandomCrystal;
+          };
+      } console.log("inside dropNewCrystals");
+      return board;
+  }
+
 GameSchema.methods.refreshBoard = function() {
-  //sends out new game board, hps, mana pools, and current player turn
+       for (var col = 0; col < SIZE; col++) {
+          for (var row = 0; row < SIZE; row++) {
+              do {
+                  //this.findAllMatches();
+                  this.checkNullSpace();      
+                  this.assignCrystalsToBoard();
+              } while (this.board[col][row] == null);
+          } 
+      } 
+
     this.io.to(this.gameId).emit('refresh-board', {homeMana: this.homeMana,
       awayMana: this.awayMana,
       gameId: this.gameId, 
@@ -46,6 +154,4 @@ GameSchema.methods.refreshBoard = function() {
       gameBoard: this.board,
       turn: this.turn
     });
-}
-
-module.exports = mongoose.model('Game', GameSchema, 'Game');
+  }
