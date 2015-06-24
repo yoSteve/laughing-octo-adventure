da40eef1efa8232b8757b032d31f3c2bc5c88c85
goofiw <goofiw@gmail.com>
2015-06-23 22:36:42 -0700

    refactor socket js to allow for intermittent team screen and to layout system for adding team members

--- routes/socket.js
+++ routes/socket.js
@@ -3,6 +3,10 @@ var User = require('../models/user');
 var Game = require('../models/game');
 var r = require('../db'); //set up db connection here
 
+
+var nspLobby;
+var games = {};
+
 var getCurrentUser = function(socket, cb){
   var cookies = socket.request.headers.cookie;
   var regEx = /passport"\:\{"user"\:"(.+?)"\}/g;
@@ -18,85 +22,90 @@ var getCurrentUser = function(socket, cb){
     }
   }
 }
-function socket (io, app, session) {
-    var nspLobby = io.of('/lobby');
-    var games = {};
-    nspLobby.on('connection', function(socket){
-      console.log('new connection, current active games: ',games.length);
-      getCurrentUser(socket, function(currentUser) {
-        if(currentUser) {
-          currentUser.socketId = socket.id;
-          currentUser.waiting = false;
-          currentUser.save();
-          User.findOne({waiting: true, username: {'$ne': currentUser.username}}, function(err, user){
-            if(user) {
-              console.log("matchXXXXXXXXXXX", user.username, currentUser.username);
-              var gameId = Game.createGameId(currentUser._id, user._id);
-              var game = Game.create({ io: nspLobby, 
-                gameId: gameId, 
-                homeUser: currentUser, 
-                awayUser: user
-              });
-              currentUser.activeGames.push(gameId);
-              user.activeGames.push(gameId);
-              //broadcast to the opponent (away)
-              socket.broadcast.to(user.socketId).emit('match-message', game.gameId);
-              //automatically joins the game room, when the opponent joins the game, he will automatically be taken to the 
-              //game page on start game
-              socket.join(game.gameId);
-              user.waiting = false;
-              user.save();
-              games[gameId] = game;
-            } else {
-            currentUser.waiting = true;
-            }
-            currentUser.save();
-          });
-          console.log(currentUser.username, " is in the lobby");
-        }
-      });
-      // joining the game lobby, initialized of game with user ids (inprogress)
-      socket.on('start-game', function(gameId){
-          socket.join(gameId);
-          games[gameId].refreshBoard();
-      });
 
-      socket.on('create-team', function(teamData) {
-        //create and add to to current user
-        });
-      socket.on('special-attack', function(attackData) {
-       //uses special attack happens mid turn  
+var enterMatchmaking = function(socket, currentUser) {
+  User.findOne({waiting: true, username: {'$ne': currentUser.username}}, function(err, user){
+    if(user) {
+      console.log("matchXXXXXXXXXXX", user.username, currentUser.username);
+      var gameId = Game.createGameId(currentUser._id, user._id);
+      var game = Game.create({ io: nspLobby, 
+        gameId: gameId, 
+        homeUser: currentUser, 
+        awayUser: user
       });
-      socket.on('move', function(data) {
-        console.log(data.gameId);
-        games[data.gameId].move(data);
-        //receives the move information
-        //emits matches to room,
-        //emits refreshed board (game state) to room 
-      });
-
-      socket.on('disconnect', function() {
-        console.log('someone left the lobby');
-        //for performance boost should store logged in users in memory
-        getCurrentUser(socket, function(currentUser) {
-          console.log('it was ', currentUser);
-            if(currentUser) {
-              currentUser.activeGames.forEach(function(game) {
-                console.log(game, 'why I print soo much');
-                if(games[game]){
-                  console.log(games[game].gameId, ' in process of being set inactive');
-                  games[game].active = false;
-                  games[game].save;
-                  delete games[game];
-                }
-              });
-              currentUser.activeGames = [];
-              currentUser.waiting = false;
-              currentUser.save();
-            }
-        });
-      });
-    });	
+      currentUser.activeGames.push(gameId);
+      user.activeGames.push(gameId);
+      //broadcast to the opponent (away)
+      socket.broadcast.to(user.socketId).emit('match-message', game.gameId);
+      //automatically joins the game room, when the opponent joins the game, he will automatically be taken to the 
+      //game page on start game
+      socket.join(game.gameId);
+      user.waiting = false;
+      user.save();
+      games[gameId] = game;
+    } else {
+    currentUser.waiting = true;
+    }
+    currentUser.save();
+  });
 }
 
-module.exports = socket;
+function nsp (io) {
+  nspLobby = io.of('/lobby'); 
+  nspLobby.on('connection', function(socket){
+    console.log('new connection, current active games: ',games.length);
+        
+    getCurrentUser(socket, function(currentUser) {
+      if(currentUser) {
+        currentUser.socketId = socket.id;
+        currentUser.waiting = false;
+        currentUser.save();
+     //socket.on('team-chosen' function(team info) {
+     //assign team info => enter matchmaking  }
+     enterMatchmaking(socket, currentUser);
+
+      }
+    });
+    // joining the game lobby, initialized of game with user ids (inprogress)
+    socket.on('start-game', function(gameId){
+        socket.join(gameId);
+        games[gameId].refreshBoard();
+    });
+
+    socket.on('special-attack', function(attackData) {
+     //uses special attack happens mid turn  
+    });
+
+    socket.on('move', function(data) {
+      console.log(data.gameId);
+      games[data.gameId].move(data);
+      //receives the move information
+      //emits matches to room,
+      //emits refreshed board (game state) to room 
+    });
+
+    socket.on('disconnect', function() {
+      console.log('someone left the lobby');
+      //for performance boost should store logged in users in memory
+      getCurrentUser(socket, function(currentUser) {
+        console.log('it was ', currentUser);
+          if(currentUser) {
+            currentUser.activeGames.forEach(function(game) {
+              console.log(game, 'why I print soo much');
+              if(games[game]){
+                console.log(games[game].gameId, ' in process of being set inactive');
+                games[game].active = false;
+                games[game].save;
+                delete games[game];
+              }
+            });
+            currentUser.activeGames = [];
+            currentUser.waiting = false;
+            currentUser.save();
+          }
+      });
+    });
+  });	
+}
+
+module.exports = nsp;
