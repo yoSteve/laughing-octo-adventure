7002f6b3ab166906ca74a49a179d8fb4df7add03
Steve Y <steve.yorke@gmail.com>
2015-06-25 21:13:09 -0700

    lovely pixel art for game background

--- public/game_js/ui/choose_team_1_button.js
+++ public/game_js/ui/choose_team_1_button.js
@@ -1,7 +1,7 @@
 game.ChooseTeam1 = me.GUI_Object.extend({
   init: function(x, y) {
     var settings = {};
-    settings.image = 'StartButton';
+    settings.image = 'team1Button';
 
     this._super(me.GUI_Object, 'init', [x, y, settings]);
   },
